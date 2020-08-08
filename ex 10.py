93ca17128de7f058e9a7c3cfd230b6fca20d148c
shubham soni <soni.shubhamknmiet@gmail.com>
2020-08-08 13:28:12 +0530

    added new pickle program

---
+++ ex 10.py
@@ -0,0 +1,26 @@
+import requests
+import pickle
+
+r  = requests.get('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data').text
+
+
+l1 = r.split('\n')
+# print(l1)
+
+l2 = [item.split(',') for item in l1 if len(item)!= 0]
+
+
+file = 'ex10.pkl'
+
+fileObj = open(file, 'wb')
+
+# pickle.dump(l2,fileObj)
+
+
+# #to read this pickle file we can use the below code
+#
+# import pickle
+# with open('ex10.pkl', 'rb') as f:
+#     print(pickle.load(f))
+
+
